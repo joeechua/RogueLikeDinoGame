94af88830fdafc095402ba78ad74ef51562f4b21
Jo Ee <joeechua123@gmail.com>
2021-04-30 18:49:11 +0800

    Added dinosaur behaviour and dinosaur capability, updated breed, grow and hunger behaviours

--- src/game/behaviours/DinosaurBehaviour.java
+++ src/game/behaviours/DinosaurBehaviour.java
@@ -3,18 +3,16 @@ package game.behaviours;
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
-import game.actors.BabyDinosaur;
 import game.actors.Dinosaur;
 
-public class GrowBehaviour implements Behaviour {
+public class DinosaurBehaviour implements Behaviour{
+
     @Override
     public Action getAction(Actor actor, GameMap map) {
-
         return null;
     }
 
-    public Action getAction(BabyDinosaur dino, GameMap map){
-        if(dino.getTurns() == )
+    public Action getAction(Dinosaur dino, GameMap map){
         return null;
     }
 }
