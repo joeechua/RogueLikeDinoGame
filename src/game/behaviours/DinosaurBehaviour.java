821f845013214716eb24255933779d7db64c3446
Chloe <cche0171@student.monash.edu>
2021-05-01 00:01:33 +0800

    update latest version

--- src/game/behaviours/DinosaurBehaviour.java
+++ src/game/behaviours/DinosaurBehaviour.java
@@ -5,8 +5,23 @@ import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 import game.actors.Dinosaur;
 
-public abstract class DinosaurBehaviour implements Behaviour{
+import java.util.ArrayList;
 
+public class DinosaurBehaviour implements Behaviour{
 
+    private ArrayList<Behaviour> bList = new ArrayList<>();
+    /*
+    if dino is hungry bList.add(Hungerbehaviour)
+    else if dino is female and not pregantn bList.add(BreedingBehaviour)
+    else if pregnant bList.add(PregnantBehaviours)
+    bList.add(WanderBehaviour)
+     */
 
+    public DinosaurBehaviour() {
+    }
+
+    @Override
+    public Action getAction(Actor actor, GameMap map) {
+        return null;
+    }
 }
