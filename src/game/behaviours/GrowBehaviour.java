ac583648abdf923b3e4b4da6a8f2c97a1bf8f4ec
Jo Ee <joeechua123@gmail.com>
2021-04-30 17:27:21 +0800

    Updated grow behaviour

---
+++ src/game/behaviours/GrowBehaviour.java
@@ -0,0 +1,21 @@
+package game.behaviours;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+
+public class GrowBehaviour implements Behaviour {
+    @Override
+    public Action getAction(Actor actor, GameMap map) {
+        /*
+        if(actor.turns == x){
+            growAction
+        }
+        else{
+            actor.turns ++;
+            return null;
+        }
+         */
+        return null;
+    }
+}
