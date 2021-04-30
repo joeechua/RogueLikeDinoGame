4fbc03f9b143413bcaa1cef622a84513acc145aa
Chloe <cche0171@student.monash.edu>
2021-04-30 20:20:56 +0800

    update bush class

--- src/game/ground/Bush.java
+++ src/game/ground/Bush.java
@@ -1,14 +1,20 @@
 package game.ground;
 
+import edu.monash.fit2099.engine.Actions;
+import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.Ground;
 import edu.monash.fit2099.engine.Location;
+import game.items.Fruit;
+
+import java.util.Random;
 
 public class Bush extends Ground{
     private int age = 0;
-    //fruits?
+    private Random random = new Random();
+    private Fruit bushFruit;
 
     public Bush() {
-        super('+');
+        super('⺾');
     }
 
     @Override
@@ -17,8 +23,17 @@ public class Bush extends Ground{
 
         age++;
         if (age == 10)
-            displayChar = 't';
+            displayChar = 'm';
         if (age == 20)
-            displayChar = 'T';
+            displayChar = 'M';
+
+        if(bushFruit == null ) {
+            bushFruit = new Fruit();
+        }
+    }
+
+    @Override
+    public Actions allowableActions(Actor actor, Location location, String direction) {
+        return new Actions(bushFruit.getHarvestAction());
     }
 }
