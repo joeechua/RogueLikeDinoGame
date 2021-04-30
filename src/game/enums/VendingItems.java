8cb7bc245c68b9e5d7b69c4c2c208842f8c4e3fd
Jo Ee <joeechua123@gmail.com>
2021-04-30 17:14:28 +0800

    Updating VendingItems and Vending Machine

---
+++ src/game/enums/VendingItems.java
@@ -0,0 +1,23 @@
+package game.enums;
+
+public enum VendingItems {
+    FRUIT(30),
+    VEGETARIAN_MEAL_KIT(100),
+    CRANIVORE_MEAL_KIT(500),
+    STEGOSAUR_EGG(200),
+    BRACHIOSAUR_EGG(500),
+    ALLOSAUR_EGG(1000),
+    LASER_GUN(500);
+
+    public final int value;
+
+    private VendingItems(int value){
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    //is this working??
+}
