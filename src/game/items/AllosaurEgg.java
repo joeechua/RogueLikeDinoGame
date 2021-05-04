f0dd6a87a9cdd7b19a282246c0c275db5b131fb8
Jo Ee <joeechua123@gmail.com>
2021-05-04 14:42:29 +0800

    Updates Egg class and subclasses with Javadoc

--- src/game/items/AllosaurEgg.java
+++ src/game/items/AllosaurEgg.java
@@ -1,26 +1,41 @@
 package game.items;
 
-import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.Location;
+import game.actors.BabyAllosaur;
+import game.actors.Player;
 
+/***
+ * Class for Allosaur Egg object
+ */
 public class AllosaurEgg extends Egg{
 
     /***
      * Constructor.
+     * @see Egg
      */
     public AllosaurEgg() {
         super("Allosaur Egg", 'Ȧ');
     }
 
+    /***
+     * Tick function to let egg keep track of time, and when egg is due to hatch
+     * @param location location of egg
+     * @see Egg
+     * @see BabyAllosaur
+     * @see Player
+     * @see Location
+     */
     @Override
     public void tick(Location location) {
         super.tick(location);
+        if(this.getTimeHatch() == 0 && !location.containsAnActor()){
+            Player.wallet.addEcoPoints(1000);
+            location.removeItem(this);
+            location.addActor(new BabyAllosaur());
+        }
     }
 
-    @Override
-    public void tick(Location location, Actor actor) {
-        super.tick(location, actor);
-    }
+
 
 
 }
--- src/game/items/Egg.java
+++ src/game/items/Egg.java
@@ -1,24 +1,28 @@
 package game.items;
 
-import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.Location;
 import game.actors.*;
 import game.enums.Gender;
 
 import java.util.Random;
-import java.util.stream.IntStream;
 
+/***
+ * Abstract class for Egg object
+ */
 public abstract class Egg extends PortableItem {
     private int timeHatch;
     private Location birthLocation;
-    private Gender gender;
-    private Random random = new Random();
+    private final Gender gender;
+    protected final Random random = new Random();
 
     /***
      * Constructor.
-     *  @param name the name of this Item
+     * @param name the name of this Item
      * @param displayChar the character to use to represent this item if it is on the ground
+     * @see Random
+     * @see Gender
+     * @see PortableItem
      */
     public Egg(String name, char displayChar) {
         super(name, displayChar);
@@ -26,35 +30,24 @@ public abstract class Egg extends PortableItem {
         this.timeHatch = 10 + random.nextInt(10);
     }
 
+    /***
+     * Tick function to let egg keep track of time, and when egg is due to hatch
+     * @param location location of egg
+     * @see Location
+     */
     public void tick(Location location) {
         super.tick(location);
-
-        if(timeHatch <= 0 && !location.containsAnActor()){
-            BabyDinosaur babyDino = null;
-            int ecoPoint = 0;
-            if(this instanceof StegosaurEgg){
-                babyDino = new BabyStegosaur();
-                ecoPoint = 100;
-            }
-            else if(this instanceof AllosaurEgg){
-                babyDino = new BabyAllosaur();
-                ecoPoint = 1000;
-            }
-            else if(random.nextInt(10) >= 3) {
-                //70% will hatch
-                babyDino = new BabyBrachiosaur();
-                ecoPoint = 1000;
-            }
-            Player.wallet.addEcoPoints(ecoPoint);
-            location.removeItem(this);
-            location.addActor(babyDino);
-        }
-        else{
-            timeHatch--;
-        }
-
+        timeHatch--;
     }
 
+    /***
+     * tick function to let egg keep track of time when it is in the player's inventory
+     * @param location location where actor is
+     * @param actor Actor that is holding an egg item
+     * @see Location
+     * @see Actor
+     * @see PortableItem
+     */
     public void tick(Location location, Actor actor) {
         super.tick(location, actor);
 
@@ -66,6 +59,12 @@ public abstract class Egg extends PortableItem {
         }
     }
 
+    /***
+     * Randomly generates a gender for the egg
+     * @return Gender enum type (F or M)
+     * @see Gender
+     * @see Random
+     */
     public Gender randGen(){
         int rand = random.nextInt(100);
         if(rand%2 == 0){
@@ -76,15 +75,20 @@ public abstract class Egg extends PortableItem {
         }
     }
 
-    public int getTimeHatch(){
-        return this.timeHatch;
-    }
-
+    /***
+     * Gives outisde classes access to location where egg was laid
+     * @param birthLocation location of where egg was laid
+     * @see Location
+     */
     public void setBirthLocation(Location birthLocation) {
         this.birthLocation = birthLocation;
     }
-    public Location getBirthLocation() {
-        return birthLocation;
-    }
 
+    /***
+     * Lets outside classes have access to the time before egg hatches
+     * @return rounds until the egg hatches
+     */
+    public int getTimeHatch() {
+        return timeHatch;
+    }
 }
