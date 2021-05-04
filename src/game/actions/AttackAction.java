37a0e5d1a774f639fee5308e8cd86262e7b97d5a
Chloe <cche0171@student.monash.edu>
2021-05-04 14:03:35 +0800

    AttackAction, DieAction updated

--- src/game/actions/AttackAction.java
+++ src/game/actions/AttackAction.java
@@ -35,71 +35,80 @@ public class AttackAction extends Action {
 		this.target = target;
 	}
 
+	/**
+	 * Perform the Attack action.
+	 *
+	 * @param actor The actor performing the action.
+	 * @param map The map the actor is on.
+	 * @return a description of what happened that can be displayed to the user.
+	 * @see Actor
+	 * @see Actions
+	 * @see Weapon
+	 * @see Dinosaur
+	 * @see Corpse
+	 */
 	@Override
 	public String execute(Actor actor, GameMap map) {
 
 		Weapon weapon = actor.getWeapon();
 
+		// there's a chance where it misses target
 		if (rand.nextBoolean()) {
 			return actor + " misses " + target + ".";
 		}
 
+		// get weapon damage
 		int damage = weapon.damage();
 		String result = "";
 
-		// decrease and increase food level for Stegosaur and Allosaurs respectively by 20
 		Dinosaur dinoTarget = (Dinosaur) target;
 		Dinosaur actorDino;
+		// if actor is dinosaur(Allosaurs)
 		if(dinoTarget.getAttackTurns() == 0 && actor instanceof Dinosaur){
-			target.hurt(damage);
+			actorDino = (Dinosaur) actor;
+			// increase and decrease hit points with damage for actor and target respectively
+			dinoTarget.hurt(damage);
 			actor.heal(damage);
+			// attacked dinosaurs food level decrease by 20
 			dinoTarget.decFoodLevel(20);
+			// baby dino food level increase by 10
 			if(actor instanceof BabyAllosaur){
-				actorDino = (Dinosaur) actor;
 				actorDino.incFoodLevel(10);
 			}
-			else if(actor instanceof Dinosaur){
-				actorDino = (Dinosaur) actor;
+			// adult dino food level increase by 10
+			else{
 				actorDino.incFoodLevel(20);
 			}
+			// set attackTurns 20
 			dinoTarget.setAttackTurns(20);
-			result = actor + " " + weapon.verb() + " " + target + " for " + damage + " damage.";
-			if (!target.isConscious()) {
-				Item corpse1 = new PortableItem("dead " + target, '%');
-				Corpse corpse = new Corpse(dinoTarget);
-				map.locationOf(target).addItem(corpse);
-
-				Actions dropActions = new Actions();
-				for (Item item : target.getInventory())
-					dropActions.add(item.getDropAction());
-				for (Action drop : dropActions)
-					drop.execute(target, map);
-				map.removeActor(target);
-
-				result += System.lineSeparator() + target + " is killed.";
+			result = actor + " " + weapon.verb() + " " + dinoTarget + " for " + damage + " damage.";
+			// if target is unconscious - die
+			if (!dinoTarget.isConscious()) {
+				result += System.lineSeparator() + new DieAction().execute(dinoTarget, map) + " and killed.";
 			}
 		}
+		// if actor is the Player
 		else if(actor instanceof Player){
+			// decrease dinoTarget hit point
 			dinoTarget.hurt(damage);
+			// if dinoTarget is unconscious - die
 			if(!dinoTarget.isConscious()){
-				Corpse corpse = new Corpse(dinoTarget);
-				map.locationOf(dinoTarget).addItem(corpse);
-				Actions dropActions = new Actions();
-				for (Item item : target.getInventory())
-					dropActions.add(item.getDropAction());
-				for (Action drop : dropActions)
-					drop.execute(target, map);
-				map.removeActor(target);
-
-				result += System.lineSeparator() + target + " is killed.";
+				result += System.lineSeparator() + new DieAction().execute(target, map) + " and killed.";
 			}
 		}
+		// when attackTurns > 0
 		else {
 			result = actor + " cannot attack " + target + " due to the remaining attack turns: " + dinoTarget.getAttackTurns() ;
 		}
 		return result;
 	}
 
+	/**
+	 * Returns a descriptive string
+	 *
+	 * @param actor The actor performing the action.
+	 * @return a description of the action
+	 */
 	@Override
 	public String menuDescription(Actor actor) {
 		return actor + " attacks " + target;
