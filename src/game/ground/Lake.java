af89ea067beeaee6a6203f7e668ad5257184d959
Chloe <cche0171@student.monash.edu>
2021-05-11 00:37:14 +0800

    Lake updated

--- src/game/ground/Lake.java
+++ src/game/ground/Lake.java
@@ -11,12 +11,18 @@ public class Lake extends Ground {
     private ArrayList<Fish> fishes;
     private int waterSips;
     private int turns;
+    private int noOfFish;
+    private int maxNoOfFish = 25;
     private Random random = new Random();
 
     public Lake(){
         super('~');
-            turns = 0;
-            waterSips = 25;
+        turns = 0;
+        waterSips = 25;
+        for(int i = 1; i <= 5; i++){
+            fishes.add(new Fish("Fish", '鱼', true));
+        }
+        noOfFish = 5;
     }
 
     @Override
@@ -26,8 +32,37 @@ public class Lake extends Ground {
 
         if (turns % 10 == 0 && random.nextDouble() <= 0.2){
             System.out.println("It's raining.");
-            double rainfall = (random.nextInt(6) + 1) * 0.1;
-            waterSips = (int) (rainfall * 20);
+            float rainfall = (float) (((random.nextInt(6)) + 1) * 0.1);
+            waterSips += (int) (rainfall * 20);
         }
+
+        if(fishes.size() < maxNoOfFish && random.nextDouble() <= 0.6){
+            fishes.add(new Fish("Fish", '鱼', true));
+            noOfFish++;
+        }
+    }
+
+    /**
+     * Get the fish array list from lake
+     * @return a fish array list from lake
+     */
+    public ArrayList<Fish> getFish() {
+        return fishes;
+    }
+
+    /**
+     * Determine whether lake contains fish or not
+     * @return a boolean, if true bush got fish, else no fish
+     */
+    public boolean gotFish(){
+        return fishes.size() > 0;
+    }
+
+    /**
+     * Determine whether lake contains water sips or not
+     * @return a boolean, if true bush got water sips, else dry
+     */
+    public boolean gotWater(){
+        return waterSips > 0;
     }
 }
